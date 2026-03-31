1bfd19f1c7976ddafcbe742d78269e6dc1f8ca58
Tiago Rosado <tr@agnosticsec.com>
2026-04-01 00:10:19 +0100

    Export set_license from licensing __init__.py
      Function exists in enforcer.py but was not re-exported from the
      package __init__.py. Backoffice entrypoint imports it at package level.

--- src/yashigani/licensing/__init__.py
+++ src/yashigani/licensing/__init__.py
@@ -1,8 +1,8 @@
 from yashigani.licensing.model import LicenseTier, LicenseState, COMMUNITY_LICENSE
 from yashigani.licensing.loader import load_license
-from yashigani.licensing.enforcer import require_feature, check_agent_limit, check_org_limit, get_license
+from yashigani.licensing.enforcer import require_feature, check_agent_limit, check_org_limit, get_license, set_license
 
 __all__ = [
     "LicenseTier", "LicenseState", "COMMUNITY_LICENSE",
-    "load_license", "require_feature", "check_agent_limit", "check_org_limit", "get_license",
+    "load_license", "require_feature", "check_agent_limit", "check_org_limit", "get_license", "set_license",
 ]
